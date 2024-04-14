9ca24dba0107e992e657963e1b068d7ce1a09d18
zjl <jialuzhang0805@gmail.com>
2024-04-15 00:41:54 +0800

    在react代码中使用主进程暴露的变量

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+const versions = window.versions;
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {`本应用正在使用 Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), 和 Electron (v${versions.electron()})`}
         </p>
         <a
           className="App-link"
